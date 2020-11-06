68b80b993daef9f5bd36d84821c4ee7217059834
René Cortez <rene.cortes@outlook.com>
2020-11-06 00:23:08 -0600

    Set template for posts content (blog)

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -24,6 +24,7 @@
 				the_content();
 				?>
 			</div>
+		</div>
 	</section>
 
 </div>
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,19 @@
 
 <?php
 	if ( is_singular() ) :
+	?>	
+		<div class="content-post">
+			<?php the_title( '<h1 class="major">', '</h1>' ); ?>
+			<?php if (get_the_post_thumbnail_url()) : ?>
+				<span class="image fit"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" /></span>
+			<?php endif; ?>
+			<div class="entry-content">
+				<?php
+				the_content();
+				?>
+			</div>
+		</div>
+	<?php
 	else :
 	?>
 		<section id="post-<?php the_ID(); ?>" class="wrapper style2 spotlights">
