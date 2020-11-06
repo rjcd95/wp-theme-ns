e9c64386fbb50ee1b35eacda8aad13c7ca253d87
René Cortez <rene.cortes@outlook.com>
2020-11-05 20:34:57 -0600

    Fix references to js files

--- functions.php
+++ functions.php
@@ -143,7 +143,7 @@ function wpthemens_scripts() {
 	wp_enqueue_style( 'wpthemens-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'wpthemens-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'wpthemens-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'wpthemens-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
