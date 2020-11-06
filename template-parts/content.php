39962653bf677deefcb7d057d6c746e7a5858851
René Cortez <rene.cortes@outlook.com>
2020-11-05 23:47:05 -0600

    Fix styles and add validations for content-page

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,18 +13,18 @@
 	if ( is_singular() ) :
 	else :
 	?>
-	<section id="post-<?php the_ID(); ?>" class="wrapper style2 spotlights">
-		<a href="<?php echo get_permalink();?>" class="image"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" data-position="center center" /></a>
-		<div class="content">
-			<div class="inner">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-				<p><?php the_excerpt(); ?></p>
-				<ul class="actions">
-					<li><a href="<?php echo get_permalink();?>" class="button">Learn more</a></li>
-				</ul>
+		<section id="post-<?php the_ID(); ?>" class="wrapper style2 spotlights">
+			<a href="<?php echo get_permalink();?>" class="image"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" data-position="center center" /></a>
+			<div class="content">
+				<div class="inner">
+					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+					<p><?php the_excerpt(); ?></p>
+					<ul class="actions">
+						<li><a href="<?php echo get_permalink();?>" class="button">Learn more</a></li>
+					</ul>
+				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 	<?php
 	
 	endif;
